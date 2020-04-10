9fbbd1a315d5ff6c99a9e1f46b682ec48fd76dd1
lucas <s3801161@student.rmit.edu.au>
2020-04-10 23:56:51 +1000

    implement some function in Player class

--- src/models/Game.java
+++ src/models/Game.java
@@ -1,21 +1,37 @@
 package models;
 
 public class Game {
-    private String gameID = idGenerator();
+    private String gameID;
     private static int uniqueID = 0;
+    private Player snakePlayer;
+    private Player humanPlayer;
     private Player[] players = new Player[2];// players[0] is snake control ID, players[1] is Human control ID
     private Board board;
     private int round;
 
     public Game(Player snakePlayer,Player humanPlayer, Board board, int round) {
-        players[0] = snakePlayer;
-        players[1] = humanPlayer;
+        this.snakePlayer = snakePlayer;
+        this.humanPlayer = humanPlayer;
+        players = new Player[]{snakePlayer, humanPlayer};
         this.board = board;
         this.round = round;
     }
 
     public Game(Player snakePlayer,Player humanPlayer, Board board) {
         this(snakePlayer,humanPlayer,board,0);
+    }
+
+    public Game() {
+        gameID = idGenerator();
+        round = 0;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
     }
 
     public String getGameID() {
@@ -33,7 +49,14 @@ public class Game {
 
     @Override
     public String toString() {
-        return String.format("Game[ ID: %s   Snake PlayerID: %s   Human PlayerID: %s ]",gameID,players[0].getId(),players[1].getId() );
-        //TODO generate boardID
+        String snake = "N/A";
+        String human = "N/A";
+        if(snakePlayer != null)
+            snake = snakePlayer.getUserID();
+        if(humanPlayer != null)
+            snake = humanPlayer.getUserID();
+        return String.format("Game[ ID: %s   Snake PlayerID: %s   Human PlayerID: %s ]", gameID, snake,human);
     }
+
+
 }
