fc459d6aaf1d1e314490310eb29f6f252608f54b
XYPeng <s3807479@student.rmit.edu.au>
2020-05-30 21:32:25 +1000

    no message

--- src/controller/BoardPane.java
+++ src/controller/BoardPane.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import model.entity.Entity;
 import model.entity.PGEntity;
 import model.entity.Position;
